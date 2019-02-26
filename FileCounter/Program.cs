62f1d7c726b350732c22e9c5c8d27878fe55900b
祐太 小松 <ky.tanabota@gmail.com>
2019-02-26 16:32:33 +0900

    first commit

---
+++ FileCounter/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace FileCounter
+{
+    class Program
+    {
+
+        private static int counter = 0;
+
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            string curDir = Directory.GetCurrentDirectory();
+            SearchFile(curDir);
+
+            sw.Stop();
+
+            File.WriteAllText(Path.Combine(curDir, "result.txt"), "Total： " + counter + "  Time : " + sw.Elapsed);
+        }
+
+        private static void SearchFile(string dirPath)
+        {
+            CountXmlFile(dirPath);
+
+            var curDirInfo = new DirectoryInfo(dirPath);
+            var dirs = curDirInfo.EnumerateDirectories("*");
+
+            foreach (DirectoryInfo dir in dirs)
+            {
+                SearchFile(dir.FullName);
+            }
+
+        }
+
+        private static void CountXmlFile(string dirPath)
+        {
+            var xmlFiles = Directory.EnumerateFiles(dirPath, "*.xml").ToArray();
+
+            foreach (string file in xmlFiles)
+            {
+                if (Path.GetFileName(file) != "info.xml")
+                {
+                    counter++;
+                }
+            }
+        }
+
+    }
+}
